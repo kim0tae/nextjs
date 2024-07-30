86d23bd4905137c5c26d2806e1c5b953ee892794
Kim youngtae <ytkim@infovine.co.kr>
2024-07-30 18:29:07 +0900

    [FEATUER] css 추가

---
+++ app/components/movie-videos.tsx
@@ -0,0 +1,12 @@
+import { stringify } from 'querystring';
+import { API_URL } from '../(home)/page';
+
+async function getVideo(id: string) {
+  const response = await fetch(`${API_URL}/${id}/videos`);
+  return response.json();
+}
+
+export default async function MovieVideos({ id }: { id: string }) {
+  const videos = await getVideo(id);
+  return <h6>{JSON.stringify(videos)}</h6>;
+}
